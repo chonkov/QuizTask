0914b80c723838c48780f27710ee9a7ba924e07b
Georgi Chonkov <georgichonkov@Georgis-MacBook-Pro.local>
2023-08-09 21:51:09 +0300

    Refactored existing tests and added one for the 'guess' function

--- test/Quiz.test.js
+++ test/Quiz.test.js
@@ -4,61 +4,87 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
 describe("Quiz", function () {
-  async function deployOneYearLockFixture() {
+  async function deployQuiz() {
     const QUESTION = "Can you guess the secret string?";
 
-    const Quiz = await ethers.getContractFactory("Quiz");
-    const quiz = await Quiz.deploy();
+    const quiz = await ethers.deployContract("Quiz");
+    // const quiz = await Quiz.deploy();
+    await quiz.waitForDeployment();
 
-    return { quiz, QUESTION };
+    const answer = await quiz.getHash("answer");
+
+    return { quiz, QUESTION, answer };
   }
 
   describe("Deployment", function () {
     it("Should return the question as a constant", async function () {
-      const { quiz, QUESTION } = await loadFixture(deployOneYearLockFixture);
+      const { quiz, QUESTION } = await loadFixture(deployQuiz);
 
       expect(await quiz.getQuestion()).to.equal(QUESTION);
     });
   });
 
   describe("Initialization", function () {
-    describe("Validations", function () {
-      it("Should have an empty answer", async function () {
-        const { quiz } = await loadFixture(deployOneYearLockFixture);
-        const answer = await quiz.answer();
+    it("Should have an empty answer", async function () {
+      const { quiz } = await loadFixture(deployQuiz);
+      const answer = await quiz.answer();
 
-        expect(answer).to.be.equal(
-          "0x0000000000000000000000000000000000000000000000000000000000000000"
-        );
+      expect(answer).to.be.equal(
+        "0x0000000000000000000000000000000000000000000000000000000000000000"
+      );
+    });
+
+    it("Should set the answer correctly", async function () {
+      const { quiz, answer } = await loadFixture(deployQuiz);
+
+      const tx = await quiz.initialize(answer);
+      await tx.wait();
+
+      expect(await quiz.answer()).to.be.equal(answer);
+    });
+
+    it("Should fail if somebody tries to change the answer", async function () {
+      const { quiz, answer } = await loadFixture(deployQuiz);
+
+      const tx = await quiz.initialize(answer);
+
+      expect(await quiz.answer()).to.be.equal(answer);
+
+      const answer2 = await quiz.getHash("answer2");
+
+      await expect(quiz.initialize(answer2)).to.be.revertedWithCustomError(
+        quiz,
+        `Quiz__SecondInitialization`
+      );
+    });
+  });
+
+  describe("Guessing", function () {
+    it("Wrong guess - no winner && funds stay within the contract", async function () {
+      const { quiz, answer } = await loadFixture(deployQuiz);
+      const [owner, other] = await ethers.getSigners();
+
+      const value = ethers.parseEther("1");
+      const answer2 = "answer2";
+
+      let tx = await quiz.initialize(answer, {
+        value: value,
       });
+      await tx.wait();
 
-      it("Should set the answer correctly", async function () {
-        const { quiz } = await loadFixture(deployOneYearLockFixture);
-        const answer = await quiz.getHash("answer");
+      expect(await quiz.answer()).to.be.equal(answer);
+      expect(await quiz.getPrizePool()).to.be.equal(value);
 
-        const tx = await quiz.initialize(answer);
-        await tx.wait();
+      tx = await quiz.guess(answer2);
+      await tx.wait();
 
-        expect(await quiz.answer()).to.be.equal(answer);
-      });
-
-      it("Should fail if somebody tries to change the answer", async function () {
-        const { quiz } = await loadFixture(deployOneYearLockFixture);
-        const answer = await quiz.getHash("answer");
-
-        const tx = await quiz.initialize(answer);
-
-        expect(await quiz.answer()).to.be.equal(answer);
-
-        const answer2 = await quiz.getHash("answer2");
-
-        await expect(quiz.initialize(answer2)).to.be.revertedWithCustomError(
-          quiz,
-          `Quiz__SecondInitialization`
-        );
-      });
+      expect(await quiz.winner()).to.be.equal(
+        "0x0000000000000000000000000000000000000000"
+      );
+      expect(await quiz.getPrizePool()).to.be.equal(value);
     });
   });
 });
